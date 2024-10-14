b972115f278e9087484e61e61ef6f1eaf39623bd
affan002 <ma08910@st.habib.edu.pk>
2024-10-14 16:11:39 +0500

    files created

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const jwt = require('jwt');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json);
+
+
+app.post('/user/signup', function(req, res) {
+
+})
+
+app.post('/user/signin', function(req, res) {
+    
+})
+
+app.get('/user/purchases', function(req, res) {
+    
+})
+
+app.get('/courses', function(req, res) {
+    
+})
+
+
+
+
+app.listen(3000)
