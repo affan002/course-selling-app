14e59d845f87ae2435cd80cee115bafd943d0aba
affan002 <ma08910@st.habib.edu.pk>
2024-10-14 17:27:44 +0500

    admin router

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const { userRouter } = require('./routes/user')
 const { courseRouter } = require('./routes/course')
+const { adminRouter } = require('./routes/admin')
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(express.json);
 
-
-app.use('/user', userRouter);
-app.use('/course', courseRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/course', courseRouter);
 
 app.listen(3000)
