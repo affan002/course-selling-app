2808be88cb49d23d86e76c361b690c188918dfdb
affan002 <ma08910@st.habib.edu.pk>
2024-10-14 17:12:09 +0500

    skeleton router files

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const jwt = require('jwt');
+const jwt = require('jsonwebtoken');
+const { userRouter } = require('./routes/user')
+const { courseRouter } = require('./routes/course')
 const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json);
 
 
-app.post('/user/signup', function(req, res) {
-
-})
-
-app.post('/user/signin', function(req, res) {
-    
-})
-
-app.get('/user/purchases', function(req, res) {
-    
-})
-
-app.get('/courses', function(req, res) {
-    
-})
-
-
-
+app.use('/user', userRouter);
+app.use('/course', courseRouter);
 
 app.listen(3000)
